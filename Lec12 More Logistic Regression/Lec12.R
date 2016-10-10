9de3ca31a26a33d8bd321b6542a9e58a4ca717af
rudeboybert <albert.ys.kim@gmail.com>
2016-10-10 08:23:21 -0400

    Done Lec12

--- Lec12 More Logistic Regression/Lec12.R
+++ Lec12 More Logistic Regression/Lec12.R
@@ -13,8 +13,10 @@ library(broom)
 
 
 
+
+
 #-------------------------------------------------------------------------------
-# Load Data & Preprocess It
+# Reload Profiles Data & Preprocess It
 #-------------------------------------------------------------------------------
 # Set the working directory of R to wherever the profiles.csv file then run. The
 # rownames_to_column(var="id") function from the tibble package adds a column
@@ -32,74 +34,96 @@ profiles <- profiles %>%
   select(-contains("essay"))
 
 # Look at our data
-View(profiles)
 glimpse(profiles)
 
 # Define a binary outcome variable is_female
 # 1 if female
 # 0 if male
 profiles <- profiles %>%
-  mutate(is_female = ifelse(sex=="f", 1, 0))
-
-# Preview of things to come:  The variable "last online" includes the time.  We
-# remove them by using the str_sub() function from the stringr package.  i.e.
-# take the "sub-string" from position 1 to 10 of each string.  Then we convert
-# them to dates.
-profiles$last_online[1:10]
-profiles <- profiles %>%
+  mutate(is_female = ifelse(sex=="f", 1, 0)) %>%
   mutate(
     last_online = stringr::str_sub(last_online, 1, 10),
     last_online = lubridate::ymd(last_online)
   )
-profiles$last_online[1:10]
+
+
 
 
 
 #---------------------------------------------------------------
-# Exercise: Scenario 1
+# Exercise: Scenario 1 - No predictors
 #---------------------------------------------------------------
 # Say you have to guess a user's sex using no information. What proportion of
 # the time should you guess female? Keep in mind, you are not making guesses
-# about a person chosen from the general population, but rather these 59946 of
-# SF OkCupid users.
+# about person chosen from the general population, but rather these 59946 of SF
+# OkCupid users.
+
+# Answer:
+mean(profiles$is_female)
+
+# Logistic regresion with intercept only; no predictors
+model_1 <- glm(is_female ~ 1, data=profiles, family="binomial")
+broom::tidy(model_1)
+
+# i.e. just an intercept
+# log(p/(1-p)) = -0.3958406 + 0
+intercept <- tidy(model_1)$estimate[1]
+
+# Solve for p. It's the same as above!
+1/(1+exp(-(  intercept  )))
 
 
 
 #---------------------------------------------------------------
-# Exercise: Scenario 2
+# Exercise: Scenario 2 - One Categorical Predictor
 #---------------------------------------------------------------
 # Now say you have access to said user's sexual orientation as listed in the
 # 3-level categorical variable orientation (note that OkCupid has since relaxed
-# the 3 choices to cover a broader spectrum).
-# CONDITIONAL on knowing a user's orientation (in other words, GIVEN that you
-# know a user's orientation), what proportion of the time should you guess
-# female?
+# the 3 choices to cover a broader spectrum of choices). CONDITIONAL on knowing
+# a user's sex, what proportion of the time should you guess female?
 
+# Answer:
+profiles %>%
+  group_by(orientation) %>%
+  summarise(prop_female=mean(is_female))
+
+# Logistic regresion with one categorical predictor: orientation
+model_2 <- glm(is_female ~ orientation, data=profiles, family="binomial")
+broom::tidy(model_2)
+
+# i.e. intercept and different "slopes" for categorical variables
+# If you're not sure how this works, ask around.
+# log(p/(1-p)) = intercept + slope
+intercept <- tidy(model_2)$estimate[1]
+slope_gay <- tidy(model_2)$estimate[2]
+slope_straight <- tidy(model_2)$estimate[3]
+
+# Solve for p. They're the same as above!
+1/(1+exp(-(  intercept  )))
+1/(1+exp(-(  intercept+slope_gay  )))
+1/(1+exp(-(  intercept+slope_straight  )))
 
 
 
 #---------------------------------------------------------------
-# Exercise: Scenario 3
+# Exercise: Scenario 3 - One Numerical Predictor
 #---------------------------------------------------------------
-# Now let's consider the numerical variable height.
-#
 # For the purposes of today's exercise only, delete the
 # -3 users who did not enter a height
 # -all users who listed "unreasonable" heights
-# Hint: check out the dplyr::between() command. Ex:
-x <- c(1:6)
-x
-between(x, 2, 5)
-
-
-
+profiles_clean <- profiles %>%
+  filter(!is.na(height)) %>%
+  filter(between(height, 55, 80))
 
 # a) Compare the heights of females and males using a density geom_histogram().
 # Set bidwidth argument in your geom_histogram so that there are no gaps
 # Notice anything weird about the distribution of male heights? Remember, these
 # are self-reported heights, not actual heights.
 
-
+# Answer
+ggplot(data=profiles_clean, aes(x=height, y=..density..)) +
+  geom_histogram(binwidth=1) +
+  facet_wrap(~sex, ncol=1)
 
 
 # b) Make a line graph where the
@@ -111,54 +135,76 @@ between(x, 2, 5)
 # -After you've made your plot, step back, take a breath, and make sure your
 # plots fit with common sense about male vs female heights
 
+# Answer
+prop_female_height <- profiles_clean %>%
+  select(is_female, height) %>%
+  group_by(height) %>%
+  summarise(prop_female = mean(is_female))
 
-# Assign your ggplot to p here:
-p <-
-
-# So that when you run the next line as is, the plot shows
+# Plot
+p <- ggplot(data=prop_female_height, aes(height, prop_female)) +
+  geom_line()
 p
 
-# c) If someone is 60 inches, are they more likely to be male or female?
-# What if they are 75 inches?
-
-
-
-
-#---------------------------------------------------------------
-# Preview for Lec12
-#---------------------------------------------------------------
-# Run the following for today. No need to understand everything; just see if
-# you can make connections with the earlier exercises
-
-# Logistic regresion with intercept only; no predictors
-model_1 <- glm(is_female ~ 1, data=profiles, family="binomial")
-broom::tidy(model_1)
-1/(1+exp(-(  -0.3944313  )))
-
-# Recognize that last number?
-
-
-
-
-# Logistic regresion with one categorical predictor: orientation
-model_2 <- glm(is_female ~ orientation, data=profiles, family="binomial")
-broom::tidy(model_2)
-1/(1+exp(-(  0.952807  )))
-1/(1+exp(-(  0.952807-1.873376*1  )))
-1/(1+exp(-(  0.952807-1.365479*1  )))
-
-# Recognize those last three numbers?
-
-
-
-
 # Logistic regresion with one numerical predictor: height
-model_3 <- glm(is_female ~ height, data=profiles, family="binomial")
+model_3 <- glm(is_female ~ height, data=profiles_clean, family="binomial")
 broom::tidy(model_3)
+
+# i.e. intercept and different "slopes", in this case intercept adjustments
+# log(p/(1-p)) = 44.4486093 - 0.6619036*height
+intercept <- tidy(model_3)$estimate[1]
+slope_height <- tidy(model_3)$estimate[2]
+
+# Solve for p, and make it a function. Note how the value of the curve
+# corresponds almost exactly to the proportion of females for any given height.
 f <- function(height) {
-  1/(1+exp(-(  44.4486093 -0.6619036*height  )))
+  1/(1+exp(-(  intercept + slope_height*height  )))
   }
 p +
   stat_function(fun = f, col="red")
 
-# See a correspondence?
+
+
+
+
+#-------------------------------------------------------------------------------
+# Get Fitted Probabilities p-hat
+#-------------------------------------------------------------------------------
+# Let's re-run all three models on the same data set for consistency
+model_1 <- glm(is_female ~ 1, data=profiles_clean, family="binomial")
+model_2 <- glm(is_female ~ orientation, data=profiles_clean, family="binomial")
+model_3 <- glm(is_female ~ height, data=profiles_clean, family="binomial")
+
+# To extract the value of the linear regression equation beta_0 + beta_1*x + ...:
+predict(model_1)
+
+# However these are clearly not probabilities. To return probabilities, i.e. by
+# applying 1/(1 + exp(-(linear regression equation))), use:
+fitted(model_1)
+
+# Create data frame of predictors, truth and predicted values
+predictions <- profiles_clean %>%
+  select(orientation, height, is_female) %>%
+  mutate(
+    p_hat1 = fitted(model_1),
+    p_hat2 = fitted(model_2),
+    p_hat3 = fitted(model_3)
+  )
+
+predictions_tidy <- predictions_wide_format %>%
+  gather(type, fitted_prob, -c(is_female, orientation, height))
+
+
+
+
+
+#-------------------------------------------------------------------------------
+# For each of our three models, compute
+#
+# -the proportion of predictions that are wrong
+# -the proportion of "female" predictions that are wrong
+# -the proportion of "male" predictions that are wrong
+#
+# Discuss amongst yourselves how you would do this.
+#-------------------------------------------------------------------------------
+
