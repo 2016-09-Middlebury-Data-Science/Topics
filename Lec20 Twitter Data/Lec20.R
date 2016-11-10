1aa261376f4814a3b1b55ede90a4d174b8b23cc0
rudeboybert <albert.ys.kim@gmail.com>
2016-11-10 09:04:15 -0500

    Done Lec20

--- Lec20 Twitter Data/Lec20.R
+++ Lec20 Twitter Data/Lec20.R
@@ -1,8 +1,18 @@
 library(twitteR)
+library(stringr)
 
-# Make sure you've run the setup_twitter_oauth() function first.
+# Note the news events below were current as of 2016/4/22!
 
-# RIP Prince:
+# Make sure you've run the setup_twitter_oauth() function once first. If you've
+# already done this, you may skip this step.
+setup_twitter_oauth(
+  "API key", 
+  "API secret", 
+  "Access token", 
+  "Access token secret"
+)
+
+# R.I.P. Prince:
 some_tweets <- searchTwitter("prince", n=100, lang="en")
 
 # The function returns a "list", not a vector, with 100 tweets.  You access them
@@ -36,11 +46,12 @@ some_tweets[[1]]$screenName
 # If we want to extract the text for each tweet, i.e. each list element, and
 # convert this to a vector of strings, we use the sapply() command.  i.e. it
 # applies the function we define to each element of the list.
-tweets.vector <- sapply(some_tweets, function(x){x$text})
-tweets.vector
+tweets_vector <- sapply(some_tweets, function(x){x$text})
+tweets_vector
 
 
 
-# EXERCISE
+# EXERCISE:
 # Look at the help file ?searchTwitter and toy with the parameters.
-# Also toy with the outputs as we did on lines 33-40.
+# Also toy with the outputs as we did on lines 33-40 using the str_ functions
+# from the stringr package.
